d53af8913bf9bc4d06c660150dfc9d150e25337a
Nick Pickering <npick001@odu.edu>
2023-12-08 16:06:06 -0500

    statistics code completed and data generated

--- generation_analysis.R
+++ generation_analysis.R
@@ -1,52 +1,41 @@
 rm(list = ls())
 
-# Load required libraries
 library(stats)
 library(tseries)
 
-# Define a function to perform the tests on a given file
-perform_tests <- function(file_path) {
-  # Read numbers from the file
+perform_tests <- function(file_path, filename) {
   numbers <- scan(file_path, what = numeric(), sep = "\n")
   
   N <- length(numbers)
-  print(N)
+  bins <- hist(numbers, 64, plot = FALSE)
   
-  # Perform chi-square test for uniformity
-  chi_square_test <- chisq.test(numbers)
-  cat("Chi-Square Test for Uniformity:\n")
-  print(chi_square_test)
+  # Adding a small random noise to break ties
+  jittered_numbers <- jitter(numbers, amount = 1e-5)
   
-  # Perform Kolmogorov-Smirnov test
-  ks_test <- ks.test(numbers, (1:N)/N)
-  cat("\nKolmogorov-Smirnov Test:\n")
-  print(ks_test)
+  chi_square_test <- chisq.test(bins$counts)
+  ks_test <- ks.test(jittered_numbers, "punif", min=min(jittered_numbers), max=max(jittered_numbers))
   
-  # Perform runs test
-  runs_test <- runs.test(numbers)
-  cat("\nRuns Test for Independence:\n")
-  print(runs_test)
+  cat("Results for file:", file_path, "\n\n", file=filename, append=TRUE)
+  cat(capture.output(chi_square_test), file=filename, append=TRUE)
+  cat("\n", file=filename, append=TRUE)
+  cat(capture.output(ks_test), file=filename, append=TRUE)
+  cat("\n--------------------------------------\n\n", file=filename, append=TRUE)
 }
 
-  # file_pattern
-  relative_path = "./DistributedNumberGeneration/Output Files/"
-  
-  # cat("Current working directory:", getwd(), "\n")
-  # cat("Files in the directory:", list.files(path = relative_path), "\n")
-  # 
-  # test_pattern <- "LEAP_FROG_proc"
-  # test_files <- list.files(path = relative_path, pattern = test_pattern)
-  # print(test_files)
-  
-  # Find files matching the pattern
-  file_list <- list.files(path = relative_path)
-  print(file_list)
-  
-  # Iterate over the files and perform tests
-  for (file_name in file_list) {
-    file_path <- paste0(relative_path, file_name)
-    cat("Processing file:", file_path, "\n")
-    perform_tests(file_path)
-    cat("\n")
-  }
+relative_path = "./DistributedNumberGeneration/Output Files/"
+output_path = "./DistributedNumberGeneration/Uniformity Tests/"
 
+file_list <- dir(path = relative_path, pattern = "stream.txt$")
+print(file_list)
+
+unlink(paste0(output_path, "LEAP_FROG_results.txt"))
+unlink(paste0(output_path, "RANDOM_TREE_results.txt"))
+
+for (file_name in file_list) {
+  file_path <- paste0(relative_path, file_name)
+  result_file <- ifelse(grepl("LEAP_FROG", file_name), "LEAP_FROG_results.txt", "RANDOM_TREE_results.txt")
+  output_filepath <- paste0(output_path, result_file)
+  
+  #cat("Processing file:", file_path, "\n")
+  perform_tests(file_path, output_filepath)
+}
